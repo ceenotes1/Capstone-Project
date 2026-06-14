0bc7368d347989b4968b0cd3ff0655e22f992ec5
sadoreena <78514743+sadoreena@users.noreply.github.com>
2026-06-13 20:28:50 -0400

    completed header.js and footer.js, added components to index.html and contact.html, deleted code from script.js

--- components/header.js
+++ components/header.js
@@ -61,6 +61,32 @@ class HeaderComponent extends HTMLElement {
                 </div>
             </header>
             `
+
+    const desktopMenuBtn = document.getElementById('desktop-menu-toggle')
+    const mobileMenuBtn = document.getElementById('mobile-menu-toggle')
+    const popUpMenu = document.getElementById('pop-up-menu')
+
+    function toggleMenu () {
+      popUpMenu.classList.toggle('open')
+    }
+
+    if (desktopMenuBtn) {
+      desktopMenuBtn.addEventListener('click', toggleMenu)
+    };
+
+    if (mobileMenuBtn) {
+      mobileMenuBtn.addEventListener('click', toggleMenu)
+    }
+
+    document.addEventListener('click', event => {
+      if (
+        !popUpMenu.contains(event.target) &&
+                !desktopMenuBtn.contains(event.target) &&
+                !mobileMenuBtn.contains(event.target)
+      ) {
+        popUpMenu.classList.remove('open')
+      }
+    })
   }
 }
 
